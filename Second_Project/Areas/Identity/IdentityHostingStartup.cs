4ca8e6a068427b42385b5dec616c01be78dc438a
marwanmar123 <youcode098@gmail.com>
2021-02-24 12:00:40 +0100

    Add project files.

---
+++ Second_Project/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Second_Project.Data;
+
+[assembly: HostingStartup(typeof(Second_Project.Areas.Identity.IdentityHostingStartup))]
+namespace Second_Project.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Second_Project/Controllers/AspNetUserRolesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Second_Project.Models;
+
+namespace Second_Project.Controllers
+{
+    public class AspNetUserRolesController : Controller
+    {
+        private readonly StudentsContext _context;
+
+        public AspNetUserRolesController(StudentsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AspNetUserRoles
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Index()
+        {
+            var studentsContext = _context.AspNetUserRoles.Include(a => a.Role).Include(a => a.User);
+            return View(await studentsContext.ToListAsync());
+        }
+
+        // GET: AspNetUserRoles/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetUserRole = await _context.AspNetUserRoles
+                .Include(a => a.Role)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (aspNetUserRole == null)
+            {
+                return NotFound();
+            }
+
+            return View(aspNetUserRole);
+        }
+
+        // GET: AspNetUserRoles/Create
+        public IActionResult Create()
+        {
+            ViewData["RoleId"] = new SelectList(_context.AspNetRoles, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.AspNetUsers, "Id", "Id");
+            return View();
+        }
+
+        // POST: AspNetUserRoles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,RoleId")] AspNetUserRole aspNetUserRole)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aspNetUserRole);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.AspNetRoles, "Id", "Id", aspNetUserRole.RoleId);
+            ViewData["UserId"] = new SelectList(_context.AspNetUsers, "Id", "Id", aspNetUserRole.UserId);
+            return View(aspNetUserRole);
+        }
+
+        // GET: AspNetUserRoles/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetUserRole = await _context.AspNetUserRoles.FindAsync(id);
+            if (aspNetUserRole == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoleId"] = new SelectList(_context.AspNetRoles, "Id", "Id", aspNetUserRole.RoleId);
+            ViewData["UserId"] = new SelectList(_context.AspNetUsers, "Id", "Id", aspNetUserRole.UserId);
+            return View(aspNetUserRole);
+        }
+
+        // POST: AspNetUserRoles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("UserId,RoleId")] AspNetUserRole aspNetUserRole)
+        {
+            if (id != aspNetUserRole.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aspNetUserRole);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AspNetUserRoleExists(aspNetUserRole.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.AspNetRoles, "Id", "Id", aspNetUserRole.RoleId);
+            ViewData["UserId"] = new SelectList(_context.AspNetUsers, "Id", "Id", aspNetUserRole.UserId);
+            return View(aspNetUserRole);
+        }
+
+        // GET: AspNetUserRoles/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aspNetUserRole = await _context.AspNetUserRoles
+                .Include(a => a.Role)
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (aspNetUserRole == null)
+            {
+                return NotFound();
+            }
+
+            return View(aspNetUserRole);
+        }
+
+        // POST: AspNetUserRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var aspNetUserRole = await _context.AspNetUserRoles.FindAsync(id);
+            _context.AspNetUserRoles.Remove(aspNetUserRole);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AspNetUserRoleExists(string id)
+        {
+            return _context.AspNetUserRoles.Any(e => e.UserId == id);
+        }
+    }
+}
